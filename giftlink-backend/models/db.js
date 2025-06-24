6a06605767ed72f5b0aea5ddf2fa16f9a8fe8d06
Fahami Jemal <fahamijemal1@gmail.com>
2025-06-25 00:20:46 +0300

    add MongoDB connection logic for Gift Service API

---
+++ giftlink-backend/models/db.js
@@ -0,0 +1,28 @@
+// db.js
+require('dotenv').config();
+const { MongoClient } = require('mongodb');
+
+// MongoDB connection URL from .env
+let url = `${process.env.MONGO_URL}`;
+
+let dbInstance = null;
+const dbName = "giftdb";
+
+async function connectToDatabase() {
+    if (dbInstance) {
+        return dbInstance;
+    }
+
+    const client = new MongoClient(url);
+
+    // Connect to MongoDB
+    await client.connect();
+
+    //Connect to database giftDB and store in variable dbInstance
+    dbInstance = client.db(dbName);
+
+    //Return database instance
+    return dbInstance;
+}
+
+module.exports = connectToDatabase;
