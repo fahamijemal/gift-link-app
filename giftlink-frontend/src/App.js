8b931e63be1da1a2f8bc536e560c4547b9a3c8fd
Fahami Jemal <fahamijemal1@gmail.com>
2025-06-26 21:41:15 +0300

    update app.js

---
+++ giftlink-frontend/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import MainPage from './components/MainPage/MainPage';
+import LoginPage from './components/LoginPage/LoginPage';
+import RegisterPage from './components/RegisterPage/RegisterPage';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+function App() {
+  const navigate = useNavigate();
+  return (
+        <>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/app" element={<MainPage />} />
+          <Route path="/app/login" element={<LoginPage/>} />
+          <Route path="/app/register" element={<RegisterPage />} />
+        </Routes>
+        </>
+  );
+}
+export default App;
